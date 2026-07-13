0084d177b1af33bfb722b9e5454ae12f69503c1c
gabrielmartins-cs <gabriel.martins@costaesilvaadv.com.br>
2026-07-13 14:04:43 -0300

    Create PalavrasTesesController.php

---
+++ PalavrasTesesController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PalavrasTeses;
+use Illuminate\Http\Request;
+
+class PalavrasTesesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\PalavrasTeses  $palavrasTeses
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PalavrasTeses $palavrasTeses)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\PalavrasTeses  $palavrasTeses
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(PalavrasTeses $palavrasTeses)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\PalavrasTeses  $palavrasTeses
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, PalavrasTeses $palavrasTeses)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\PalavrasTeses  $palavrasTeses
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PalavrasTeses $palavrasTeses)
+    {
+        //
+    }
+}
